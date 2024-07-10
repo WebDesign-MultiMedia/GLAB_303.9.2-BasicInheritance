82697c510b47a9ab3a0b04f645bb1dac0cc51dc0
Julio <youremail@example.com>
2024-07-10 14:35:55 -0400

    GLAB_303.9.2

---
+++ src/Hospital.java
@@ -0,0 +1,25 @@
+public class Hospital {
+    public static void main(String[] args) {
+
+        Surgeon s = new Surgeon();
+        s.Doctor_Details();
+        s.Surgeon_Details();
+
+        /**
+         * Child Class:
+         * that extends the features of another class is known as a
+         * (child, subclass or derived class.)
+         * */
+
+        /**
+         * Parent Class:
+         * whoes properties and functionalities are used (inherited) by
+         * another class is known as the
+         * (parent class, superclass, or base class)
+         * **/
+
+
+
+
+    }
+}
---
+++ src/Surgeon.java
@@ -0,0 +1,7 @@
+public class Surgeon extends Doctor{
+    void Surgeon_Details(){
+        System.out.println("Surgeon Details... ");
+        System.out.println(Department = "Cardio");
+        System.out.println("Run");
+    }
+}
